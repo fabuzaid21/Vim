f9e52513f7205bd524af8dca03662e3dfae94bd2
Firas Abuzaid <fabuzaid21@gmail.com>
2017-04-28 18:52:02 -0700

    Additional changes to install script

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+set -x
+
 # sudo apt-get install build-essential cmake python-dev python3-dev
+
+cd $HOME
+rm -rf .vim .vimrc
+ln -s Vim/.vimrc
+ln -s Vim/.vim
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
+
